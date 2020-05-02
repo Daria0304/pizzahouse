483bc73e06f1a834bc09833d8bcf8d308a7e6fee
Daria Szubierajska <d.szubierajska@gmail.com>
2020-05-02 15:18:02 -0300

    mvc setup

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Pizza;
+
+class PizzaController extends Controller
+{
+  public function index() {
+    //$pizzas = Pizza::all();
+    //$pizzas = Pizza::orderBy('name', 'desc')->get();
+    //$pizzas = Pizza::where('type', 'hawaiian')->get();
+    $pizzas = Pizza::latest()->get();
+
+
+    return view('pizzas.index', [
+      'pizzas' => $pizzas,
+    ]);
+  }
+
+  public function show($id) {
+    return view('pizzas.show', ['id' => $id]);
+  }
+
+  public function create() {
+    return view('pizzas.create');
+  }
+
+}
